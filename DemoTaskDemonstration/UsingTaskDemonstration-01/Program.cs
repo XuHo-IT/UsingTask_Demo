8159f6aac8fab8ac3ea5b14a53db5f9832afdef3
XuHo-IT <ngotranxuanhoa09062004@gmail.com>
2025-09-18 19:41:22 +0700

    first commit

---
+++ DemoTaskDemonstration/UsingTaskDemonstration-01/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void PrintNumber(String message)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"{message}:{i}");
+            Thread.Sleep(1000);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Thread.CurrentThread.Name = "Main";
+        Task task1 = new Task(() => PrintNumber("Task 01"));  
+        task1.Start();
+        Task task2 = Task.Run(delegate { PrintNumber("Task 02"); });
+        Task task3 = new Task(new Action(() => PrintNumber("Task 03")));
+        task3.Start();
+        Console.WriteLine($"Thread '{Thread.CurrentThread.Name}'");
+        Console.ReadKey();
+    }
+}
